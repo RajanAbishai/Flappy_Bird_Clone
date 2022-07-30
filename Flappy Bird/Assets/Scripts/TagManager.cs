a1a9d358c583662b0a0c0e4528c70ed02f3e2ba6
Rajan <themailboxofabi@yahoo.com>
2022-07-30 19:03:48 +0530

    Gameplay scene created
    Tag Manager Created, Ground sprite sliced so that it can be snapped to fit with other ground sprites (duplicate game objects). 8 background + ground combinations created

---
+++ Flappy Bird/Assets/Scripts/TagManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagManager : MonoBehaviour
+{
+
+    public static string PLAYER_TAG = "Player";
+    public static string FLAP_BUTTON_TAG= "FlapButton";
+    public static string BACKGROUND_TAG="Background";
+    public static string GROUND_TAG="Ground";
+    public static string PIPE_TAG="Pipe";
+    public static string PIPE_HOLDER_TAG="PipeHolder";
+
+
+}
