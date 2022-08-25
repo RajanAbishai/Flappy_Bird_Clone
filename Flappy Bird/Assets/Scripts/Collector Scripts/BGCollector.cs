f2f2d62c03589edbad3bb316904b0f5292e1a85e
Rajan <themailboxofabi@yahoo.com>
2022-08-26 05:28:50 +0530

    Pipecollector is not working properly.
    The collision between the pipe collector and the pipe holder isn't being recognized.

---
+++ Flappy Bird/Assets/Scripts/Collector Scripts/BGCollector.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGCollector : MonoBehaviour
+{
+
+
+    private GameObject[] backgrounds;
+    private GameObject[] grounds;
+
+
+    private float lastBGX;
+    private float lastGroundX;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        backgrounds = GameObject.FindGameObjectsWithTag(TagManager.BACKGROUND_TAG);
+        grounds = GameObject.FindGameObjectsWithTag(TagManager.GROUND_TAG);
+
+        lastBGX = backgrounds[0].transform.position.x; //last BGX will be the first background the BG collector collides with
+        lastGroundX = grounds[0].transform.position.x; //last Ground will be the first ground the BG collector collides with
+
+        /*We are going to loop through our arrays and compare the position of our backgrounds and grounds and assign them to our lastBGX
+         and lastGroundX*/
+
+        for (int i= 1; i < backgrounds.Length; i++)
+        {
+            if (lastBGX < backgrounds[i].transform.position.x)
+            {
+                lastBGX = backgrounds[i].transform.position.x; //reassign the position
+            }
+
+        }
+
+
+
+        for (int i = 1; i < grounds.Length; i++)
+        {
+            if (lastGroundX < grounds[i].transform.position.x)
+            {
+                lastGroundX = grounds[i].transform.position.x; //reassign the position
+            }
+
+        }
+
+
+
+
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == TagManager.BACKGROUND_TAG)
+        {
+            Vector3 temp = target.transform.position;
+            float width = ((BoxCollider2D)target).size.x;
+
+            temp.x = lastBGX + width; //width of the background (like BGday)
+
+            target.transform.position = temp;
+
+            lastBGX = temp.x;
+        }
+
+        else if (target.tag == TagManager.GROUND_TAG)
+            {
+                Vector3 temp = target.transform.position;
+                float width = ((BoxCollider2D)target).size.x;
+
+                temp.x = lastGroundX + width; //width of the ground 
+
+                target.transform.position = temp;
+
+                lastGroundX = temp.x;
+            }
+
+
+    }
+
+
+
+}
---
+++ Flappy Bird/Assets/Scripts/Collector Scripts/PipeCollector.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeCollector : MonoBehaviour
+{
+
+    private GameObject[] pipeHolders;
+    private float distance = 2.5f;
+    private float lastPipeX;
+
+    /*pipemin and max are going to be for the y axis. Basically to decide the range where the pipe can spawn?*/
+    private float pipeMin=-1.5f; 
+    private float pipeMax=2.4f;
+
+    private void Awake()
+    {
+        pipeHolders = GameObject.FindGameObjectsWithTag(TagManager.PIPE_HOLDER_TAG);
+
+        for(int i = 0; i < pipeHolders.Length; i++)
+        {
+            Vector3 temp = pipeHolders[i].transform.position;
+            temp.y = Random.Range(pipeMin, pipeMax);
+            pipeHolders[i].transform.position = temp;
+        }
+        
+        lastPipeX = pipeHolders[0].transform.position.x;
+
+
+        for (int i = 1; i < pipeHolders.Length; i++)
+        {
+            if (lastPipeX < pipeHolders[i].transform.position.x)
+            {
+                lastPipeX = pipeHolders[i].transform.position.x;
+            }
+
+        }
+
+
+
+
+    }
+
+
+
+    
+
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == TagManager.PIPE_HOLDER_TAG)
+        {
+            print("Pipe collector collided with the pipe holder"); 
+            Vector3 temp = target.transform.position;
+
+            temp.x = lastPipeX+distance;
+            temp.y = Random.Range(pipeMin, pipeMax);
+
+            target.transform.position = temp;
+
+            lastPipeX = temp.x; //to re-assign the last position X to the variable
+        }
+
+
+    }
+
+
+
+}
